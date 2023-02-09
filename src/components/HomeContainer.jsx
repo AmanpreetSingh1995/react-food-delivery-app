6b6e582ddd4bad9ec75c29735ccb1d315aceb99f
Amanpreet Singh <amanpreetsingh369@gmail.com>
2023-02-09 12:40:10 +0530

    HomeContainer

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import Delivery from "../img/delivery.png";
+import HeroBg from "../img/heroBg.png";
 
 const HomeContainer = () => {
 	return (
-		<section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full">
+		<section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
 			<div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
 				<div
 					className="flex items-center gap-2 justify-center bg-orange-100
@@ -42,7 +43,11 @@ const HomeContainer = () => {
 					Order Now
 				</button>
 			</div>
-			<div className="py-2 bg-blue-400 flex-1"></div>
+			<div className="py-2 flex-1 flex items-center">
+				<img src={HeroBg} className="ml-auto h-650" alt="hero-bg" />
+
+				<div className="w-full h-full absolute flex items-center justify-center"></div>
+			</div>
 		</section>
 	);
 };
