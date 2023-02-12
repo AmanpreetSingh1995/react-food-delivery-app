8ea227679b8e18c225d3d3a15a79c33d5f2b0e9f
Amanpreet Singh <amanpreetsingh369@gmail.com>
2023-02-12 12:05:22 +0530

    Create Container

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const CreateContainer = () => {
 	return (
-		<div className="w-full h-auto flex items-center justify-center">
-			<div className="w-[90%] md:w-[75%] border border-gray-200 rounded-lg p-4"></div>
+		<div className="w-full min-h-screen flex items-center justify-center">
+			<div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4"></div>
 		</div>
 	);
 };
