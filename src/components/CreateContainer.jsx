61fce293c23d01fb9ca58bd88423f1d65ca0d6f2
Amanpreet Singh <amanpreetsingh369@gmail.com>
2023-02-17 18:28:47 +0530

    Create Container

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-import { MdFastfood, MdCloudUpload } from "react-icons/md";
+import {
+	MdFastfood,
+	MdCloudUpload,
+	MdDelete,
+	MdFoodBank,
+	MdAttachMoney,
+} from "react-icons/md";
 
 import { categories } from "../utils/data";
 import Loader from "./Loader";
@@ -18,6 +24,8 @@ const CreateContainer = () => {
 	const [isLoading, setIsLoading] = useState(false);
 
 	const uploadImage = () => {};
+	const deleteImage = () => {};
+	const saveDetails = () => {};
 
 	return (
 		<div className="w-full min-h-screen flex items-center justify-center">
@@ -45,7 +53,7 @@ const CreateContainer = () => {
 						value={title}
 						onChange={(e) => setTitle(e.target.value)}
 						placeholder="Give me a title..."
-						className="w-full h-full text-xl bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor"
+						className="w-full h-full text-xl bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
 					/>
 				</div>
 
@@ -95,10 +103,61 @@ const CreateContainer = () => {
 									</label>
 								</>
 							) : (
-								<></>
+								<>
+									<div className="relative h-full">
+										<img
+											src={imageAsset}
+											alt="uploaded image"
+											className="w-full h-full object-cover"
+										/>
+										<button
+											type="button"
+											className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-100 transition-all ease-in-out"
+											onClick={deleteImage}
+										>
+											<MdDelete className="text-white" />
+										</button>
+									</div>
+								</>
 							)}
 						</>
 					)}
+				</div>
+
+				<div className="w-full flex flex-col md:flex-row items-center gap-3">
+					<div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+						<MdFoodBank className="text-gray-700 text-2xl" />
+						<input
+							type="text"
+							required
+							value={calories}
+							onChange={(e) => setCalories(e.target.value)}
+							placeholder="Calories"
+							className="w-full h-full text-xl bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+						/>
+					</div>
+
+					<div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+						<MdAttachMoney className="text-gray-700 text-2xl" />
+						<input
+							type="text"
+							required
+							value={price}
+							onChange={(e) => setPrice(e.target.value)}
+							placeholder="Price"
+							className="w-full h-full text-xl bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+						/>
+					</div>
+				</div>
+
+				<div className="flex items-center w-full">
+					<button
+						type="button"
+						className="ml-0 md:ml-auto w-full md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-lg text-white font-semibold"
+						onClick={saveDetails}
+					>
+						Save
+					</button>
 				</div>
 			</div>
 		</div>
