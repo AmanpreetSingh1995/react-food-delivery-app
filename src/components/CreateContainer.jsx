a4022d78627ca0bf8231841cd353b48b91828cb9
Amanpreet Singh <amanpreetsingh369@gmail.com>
2023-02-17 12:26:34 +0530

    Create Container

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-import { MdFastfood } from "react-icons/md";
+import { MdFastfood, MdCloudUpload } from "react-icons/md";
 
 import { categories } from "../utils/data";
 import Loader from "./Loader";
@@ -15,7 +15,9 @@ const CreateContainer = () => {
 	const [fields, setFields] = useState(false);
 	const [alertStatus, setAlertStatus] = useState("danger");
 	const [msg, setMsg] = useState(null);
-	const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const uploadImage = () => {};
 
 	return (
 		<div className="w-full min-h-screen flex items-center justify-center">
@@ -70,7 +72,33 @@ const CreateContainer = () => {
 				</div>
 
 				<div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg">
-					{isLoading ? <Loader /> : <></>}
+					{isLoading ? (
+						<Loader />
+					) : (
+						<>
+							{!imageAsset ? (
+								<>
+									<label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+										<div className="w-full h-full flex flex-col items-center justify-center cursor-pointer gap-2">
+											<MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+											<p className="text-gray-500 hover:text-gray-700">
+												Click here to upload
+											</p>
+										</div>
+										<input
+											type="file"
+											name="uploadimage"
+											accept="image/*"
+											onChange={uploadImage}
+											className="w-0 h-0"
+										/>
+									</label>
+								</>
+							) : (
+								<></>
+							)}
+						</>
+					)}
 				</div>
 			</div>
 		</div>
